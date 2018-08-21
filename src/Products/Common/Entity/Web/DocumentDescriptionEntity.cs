f479340d9f1f0fbe5b89fdfcf4fc9249db25210b
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2018-08-21 08:23:27 +0300

    Fixed FireFox thumbnails issue

---
+++ src/Products/Common/Entity/Web/DocumentDescriptionEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace GroupDocs.Viewer.WebForms.Products.Common.Entity.Web
+{
+    /// <summary>
+    /// DocumentDescriptionEntity
+    /// </summary>
+    public class DocumentDescriptionEntity
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public int number { get; set; }
+    }
+}
